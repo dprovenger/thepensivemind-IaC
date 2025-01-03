0639bfeba58ee8667b6d6f6ed28696a8c4dc3447
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2025-01-02 20:06:42 -0600

    Adding association of elastic IP to ec2 instance

--- main.tf
+++ main.tf
@@ -36,16 +36,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+# Associating existing EIP to new instance
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web-thepensivemind.subnet_id
+  allocation_id = eipalloc-0d10cf5a6edc29356
+}
+
 # EC2 web server instance creation
 resource "aws_instance" "web-thepensivemind" {
-  ami = data.aws_ami.ubuntu.AWS_ACCESS_KEY_ID
-  instance_type = "t2.micro"
-  key_name = "dev-linux-thepensivemind"
-  subnet_id = "subnet-fa80fc9c"
+  ami                    = data.aws_ami.ubuntu.AWS_ACCESS_KEY_ID
+  instance_type          = "t2.micro"
+  key_name               = "dev-linux-thepensivemind"
+  subnet_id              = "subnet-fa80fc9c"
   vpc_security_group_ids = ["sg-0ea1c87624830a457"]
 
   tags = {
     "Environment" = "thepensivemind.com"
-    "Terraform" = "true"
+    "Terraform"   = "true"
   }
 }
