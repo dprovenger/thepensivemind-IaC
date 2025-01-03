1542589caa6bf906ef698f25925b8be098ee6a91
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2025-01-03 05:33:03 -0600

    Adding SSL cert to Nginx

--- ubuntuUserData.sh
+++ ubuntuUserData.sh
@@ -7,10 +7,19 @@
 # Update the package index
 sudo apt update
 
-# Installing, starting, and enabling nginx
+# Installing, starting, and enabling Nginx
 sudo apt install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
+
+# Installing certbot to enble https in Nginx
+sudo apt install -y certbot python3-certbot-nginx
+
+# Run Certbot with Nginx to obtain and install SSL cert
+sudo certbot --nginx -d aw.thepensivemind.com --non-interactive --agree-tos --email dprovenger@yahoo.com
+
+# Reload Nginx
+sudo systemctl reload nginx
 
 # Installing networking utilities
 sudo apt -y install net-tools 
